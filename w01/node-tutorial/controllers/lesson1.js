72b7b2b447d7c384aebdcf1378fd8df7f20cde76
ktakamasa <kohtakamasa@gmail.com>
2023-06-01 23:57:08 +0100

    fix order

--- w01/node-tutorial/controllers/lesson1.js
+++ w01/node-tutorial/controllers/lesson1.js
@@ -1,17 +1,17 @@
-const kohRoute = (req, res) => {
-    res.send("Koh Takamasa");
-};
-
 const kazRoute = (req, res) => {
-    res.send("Kaz Takamasa");
+  res.send("Kaz Takamasa");
 };
 
 const jaquelineRoute = (req, res) => {
-    res.send("Jaqueline Takamasa");
+  res.send("Jaqueline Takamasa");
+};
+
+const kohRoute = (req, res) => {
+  res.send("Koh Takamasa");
 };
 
 module.exports = {
-    kohRoute,
-    jaquelineRoute,
-    kazRoute
+  kazRoute,
+  jaquelineRoute,
+  kohRoute,
 };
