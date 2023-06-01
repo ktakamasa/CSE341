1efd0a1e16faf04eeff77b39cd550e2a157de0a6
ktakamasa <kohtakamasa@gmail.com>
2023-06-01 23:42:59 +0100

    added names

--- w01/node-tutorial/routes/index.js
+++ w01/node-tutorial/routes/index.js
@@ -1,7 +1,8 @@
 const routes = require("express").Router();
 const lesson1Controller = require("../controllers/lesson1");
 
-routes.get("/", lesson1Controller.kohRoute);
+routes.get("/", lesson1Controller.kazRoute);
 routes.get("/jaqueline", lesson1Controller.jaquelineRoute);
+routes.get("/kaz", lesson1Controller.kazRoute);
 
 module.exports = routes;
