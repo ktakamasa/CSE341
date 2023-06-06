8f99656d0a854636619403b3d25ed7b550f4ac57
ktakamasa <kohtakamasa@gmail.com>
2023-06-06 16:24:58 +0100

    mongodb connect to node demo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use("/", require("./routes/index"));
 
-app.listen(process.env.port || port);
-console.log("Web Server is listening at port " + (process.env.port || port));
+app.listen(port);
+console.log("Web Server is listening at port " + port);
