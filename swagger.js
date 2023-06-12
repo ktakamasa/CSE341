629bf0ec998854e5cf989660e5d48c658214a7e3
ktakamasa <kohtakamasa@gmail.com>
2023-06-12 23:00:51 +0100

    remove http

--- swagger.js
+++ swagger.js
@@ -7,7 +7,7 @@ const doc = {
     description: 'Contacts API'
   },
   host: 'cse341-ktakamasa.onrender.com',
-  schemes: ['https', 'http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
