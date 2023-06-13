cf5b9262571fd7d09f1e1a1ea461f6681eb7fb35
ktakamasa <kohtakamasa@gmail.com>
2023-06-13 15:54:12 +0100

    one more test

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.use((req, res, next) => {
   //   'Access-Control-Allow-Headers',
   //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
   // );
-  // res.setHeader('Content-Type', 'application/json');
-  // res.setHeader(
-  //   'Access-Control-Allow-Methods',
-  //   'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-  // );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
   next();
 });
 app.use('/', require('./routes'));
