d0433f54a8d124e8e4a3291c48fde1e75ac7c3fc
ktakamasa <kohtakamasa@gmail.com>
2023-06-13 21:38:18 +0100

    api doc

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-router.use('/docs', swaggerUi.serve);
-router.get('/docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
