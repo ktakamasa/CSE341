95dbd4259e113f8a95a4832db68ab8dff57b34f4
ktakamasa <kohtakamasa@gmail.com>
2023-06-14 00:28:06 +0100

    one more

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
-const app = express();
 const port = process.env.port || 3000;
+const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Content-Type', 'application/json', 'text/html');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-  );
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  // );
+  // res.setHeader('Content-Type', 'application/json', 'text/html');
+  // res.setHeader(
+  //   'Access-Control-Allow-Methods',
+  //   'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  // );
   next();
 });
 app.use('/', require('./routes'));
